6b74b13ce021e8440fef323c1d0f4cb425d6efd3
Joson <3761011+aaceshi@users.noreply.github.com>
2021-02-07 23:12:30 +0800

    Create x64_19.07.sh

---
+++ x64_19.07.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo '下载ServerChan'
+git clone https://github.com/tty228/luci-app-serverchan.git package/openwrt-packages/luci-app-serverchan
+
+echo '下载bandwidthd'
+git clone https://github.com/AlexZhuo/luci-app-bandwidthd.git package/openwrt-packages/luci-app-bandwidthd
+
+echo '下载eqos'
+git clone https://github.com/garypang13/luci-app-eqos.git package/openwrt-packages/luci-app-eqos
+
+echo '下载jd签到'
+git clone https://github.com/jerrykuku/node-request.git package/openwrt-packages/node-request
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/openwrt-packages/luci-app-jd-dailybonus
+
+echo '下载系统高级设置'
+svn co https://github.com/siropboy/mypackages/trunk/luci-app-advanced package/openwrt-packages/luci-app-advanced
+
+echo '下载kpr+'
+git clone https://github.com/cnzd/luci-app-koolproxyR.git package/openwrt-packages/luci-app-koolproxyR
+
+echo '替换UPNP为2.0.20170421'
+rm -rf feeds/packages/net/miniupnpd
+svn co https://github.com/coolsnowwolf/packages/trunk/net/miniupnpd feeds/packages/net/miniupnpd
