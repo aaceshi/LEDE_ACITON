cfd56fc555a4e66e006bce02fccf893bc227c18e
Joson <3761011+aaceshi@users.noreply.github.com>
2021-02-08 09:52:34 +0800

    Update x64_19.07.sh

--- x64_19.07.sh
+++ x64_19.07.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
-sed -i '$a src-git opentopd https://github.com/sirpdboy/sirpdboy-package' feeds.conf.default
+#sed -i '$a src-git opentopd https://github.com/sirpdboy/sirpdboy-package' feeds.conf.default
 
 echo '下载ServerChan'
 git clone https://github.com/tty228/luci-app-serverchan.git package/openwrt-packages/luci-app-serverchan
@@ -16,11 +16,11 @@ git clone https://github.com/AlexZhuo/luci-app-bandwidthd.git package/openwrt-pa
 #git clone https://github.com/jerrykuku/node-request.git package/openwrt-packages/node-request
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/openwrt-packages/luci-app-jd-dailybonus
 
-#echo '下载系统高级设置'
-#svn co https://github.com/siropboy/mypackages/trunk/luci-app-advanced package/openwrt-packages/luci-app-advanced
+echo '下载系统高级设置'
+svn co https://github.com/siropboy/mypackages/trunk/luci-app-advanced package/openwrt-packages/luci-app-advanced
 
-#echo '下载kpr+'
-#git clone https://github.com/cnzd/luci-app-koolproxyR.git package/openwrt-packages/luci-app-koolproxyR
+echo '下载kpr+'
+git clone https://github.com/cnzd/luci-app-koolproxyR.git package/openwrt-packages/luci-app-koolproxyR
 
 echo '替换UPNP为2.0.20170421'
 rm -rf feeds/packages/net/miniupnpd
