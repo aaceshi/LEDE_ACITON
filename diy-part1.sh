753eb711ab85db6c436da955a5a37588fa695624
Joson <3761011+aaceshi@users.noreply.github.com>
2021-02-08 12:03:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,15 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+git clone https://github.com/superstarfly/openwrt-package package/lienol
+git clone https://github.com/superstarfly/OpenClash package/openclash
+git clone https://github.com/jefferymvp/luci-app-koolproxyR package/koolproxyR
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/adguardhome
+git clone https://github.com/frainzy1477/luci-app-clash package/clash
+git clone https://github.com/fw876/helloworld package/ssrplus
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
