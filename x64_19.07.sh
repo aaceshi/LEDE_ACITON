b7406b9df2df3b0d30db0680d9a7a005794bd645
Joson <3761011+aaceshi@users.noreply.github.com>
2021-02-08 12:07:17 +0800

    Update x64_19.07.sh

--- x64_19.07.sh
+++ x64_19.07.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-#sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
-#sed -i '$a src-git opentopd https://github.com/sirpdboy/sirpdboy-package' feeds.conf.default
+git clone https://github.com/superstarfly/openwrt-package package/lienol
+git clone https://github.com/superstarfly/OpenClash package/openclash
+git clone https://github.com/jefferymvp/luci-app-koolproxyR package/koolproxyR
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/adguardhome
+git clone https://github.com/frainzy1477/luci-app-clash package/clash
+git clone https://github.com/fw876/helloworld package/ssrplus
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 
-echo '下载ServerChan'
 git clone https://github.com/tty228/luci-app-serverchan.git package/openwrt-packages/luci-app-serverchan
 
-echo '下载bandwidthd'
 git clone https://github.com/AlexZhuo/luci-app-bandwidthd.git package/openwrt-packages/luci-app-bandwidthd
 
-#echo '下载eqos'
-#git clone https://github.com/garypang13/luci-app-eqos.git package/openwrt-packages/luci-app-eqos
+git clone https://github.com/garypang13/luci-app-eqos.git package/openwrt-packages/luci-app-eqos
 
 git clone https://github.com/jerrykuku/node-request.git package/openwrt-packages/node-request
-#echo '下载jd签到'
-#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/openwrt-packages/luci-app-jd-dailybonus
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/openwrt-packages/luci-app-jd-dailybonus
 
-echo '下载系统高级设置'
 svn co https://github.com/siropboy/mypackages/trunk/luci-app-advanced package/openwrt-packages/luci-app-advanced
 
-echo '下载kpr+'
 git clone https://github.com/cnzd/luci-app-koolproxyR.git package/openwrt-packages/luci-app-koolproxyR
 
-echo '替换UPNP为2.0.20170421'
 rm -rf feeds/packages/net/miniupnpd
 svn co https://github.com/coolsnowwolf/packages/trunk/net/miniupnpd feeds/packages/net/miniupnpd
