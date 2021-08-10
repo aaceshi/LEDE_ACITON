73328f83224bf246b26c8daa7e1457762a058f5c
Joson <3761011+aaceshi@users.noreply.github.com>
2021-08-10 17:27:11 +0800

    Update x64_19.07.sh

--- x64_19.07.sh
+++ x64_19.07.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
-git clone https://github.com/superstarfly/openwrt-package package/lienol
-git clone https://github.com/superstarfly/OpenClash package/openclash
-git clone https://github.com/jefferymvp/luci-app-koolproxyR package/koolproxyR
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/adguardhome
-git clone https://github.com/frainzy1477/luci-app-clash package/clash
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/fw876/helloworld package/ssrplus
-git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
-
-git clone https://github.com/tty228/luci-app-serverchan.git package/openwrt-packages/luci-app-serverchan
-
-git clone https://github.com/AlexZhuo/luci-app-bandwidthd.git package/openwrt-packages/luci-app-bandwidthd
-
-git clone https://github.com/garypang13/luci-app-eqos.git package/openwrt-packages/luci-app-eqos
-
-git clone https://github.com/jerrykuku/node-request.git package/openwrt-packages/node-request
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/openwrt-packages/luci-app-jd-dailybonus
-
-svn co https://github.com/siropboy/mypackages/trunk/luci-app-advanced package/openwrt-packages/luci-app-advanced
-
-git clone https://github.com/cnzd/luci-app-koolproxyR.git package/openwrt-packages/luci-app-koolproxyR
-
-rm -rf feeds/packages/net/miniupnpd
-svn co https://github.com/coolsnowwolf/packages/trunk/net/miniupnpd feeds/packages/net/miniupnpd
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+git clone https://github.com/pexcn/openwrt-chinadns-ng package/chinadns-ng
+git clone https://github.com/izilzty/luci-app-chinadns-ng package/luci-app-chinadns-ng
