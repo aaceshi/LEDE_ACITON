a10ffb09113e26b89c2b3e5e8532002cb59be44d
Joson <3761011+aaceshi@users.noreply.github.com>
2021-08-10 21:17:45 +0800

    Update x64_19.07.sh

--- x64_19.07.sh
+++ x64_19.07.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/fw876/helloworld package/ssrplus
+git clone https://github.com/imapproxy/luci-app-turboacc package/luci-app-turboacc
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 git clone https://github.com/pexcn/openwrt-chinadns-ng package/chinadns-ng
